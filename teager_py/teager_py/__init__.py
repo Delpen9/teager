e47f9b11d687df08066a6b8ae270aac24b903668
Delpen9 <iantdover@gmail.com>
2018-12-19 15:43:56 -0600

    The bulk of the code

---
+++ teager_py/teager_py/__init__.py
@@ -0,0 +1,6 @@
+import numpy as numpy
+from .teager import Teager
+from .teager import horizontal_teager
+from .teager import vertical_teager
+from .teager import diagonal_teager_right
+from .teager import diagonal_teager_left
