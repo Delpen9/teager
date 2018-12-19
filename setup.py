8dddb47250ea8c3c291fd03f10acaa861d96308d
Delpen9 <iantdover@gmail.com>
2018-12-19 14:15:18 -0600

    New Setup File
    Allows one one to download this package locally.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'teager',
+      version = '0.1',
+      description = 'A module to cleanly run a Teager operation.',
+      url = 'https://github.com/Delpen9/teager',
+      author = 'Ian Dover',
+      author_email = 'iantdover@gmail.com',
+      license = 'MIT',
+      packages = ['teager'],
+      zip_safe = False)
