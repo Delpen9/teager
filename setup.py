9bf7228504f9615a03f5ef7c5914d9ab28b0f174
Delpen9 <iantdover@gmail.com>
2018-12-23 12:02:47 -0600

    Updated Setup

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(name = 'teager_py',
       version = '0.4',
-      description = 'A module to cleanly run a Teager operation.',
-      url = 'https://github.com/Delpen9/teager_py',
+      description = 'A module to cleanly run a Teager-Kaiser Energy Operation.',
+      url = 'https://github.com/Delpen9/teager',
       author = 'Ian Dover',
       author_email = 'iantdover@gmail.com',
       license = 'MIT',
